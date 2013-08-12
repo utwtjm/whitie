73b3d15af449d2700c26cdad79560e89cc3009bf
utwtjm <utwtjm@gmail.com>
2013-08-12 22:37:17 +0800

    test

---
+++ application/config/image.php
@@ -0,0 +1,13 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+// ==================================================================
+//
+// image_service setting
+//
+// ------------------------------------------------------------------
+
+$config['image_library'] = 'gd2';
+$config['quality'] = '90%';
+$config['create_thumb'] = false;
+$config['maintain_ratio'] = false;
+$config['master_dim'] = 'auto';
